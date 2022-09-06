8b505ce3a426eefced27ba6bf1d79fbc8b35f1c5
Luis Coelho <mancoeluis@gmail.com>
2022-09-06 18:34:17 -0300

    🐛 fix kafka connection with docker

--- app/Kafka/kafkaConnector.ts
+++ app/Kafka/kafkaConnector.ts
@@ -1,8 +1,10 @@
 import { Kafka } from 'kafkajs'
 
+const myBroker = process.env.KAFKA_BROKER_IP || 'localhost:9091'
+
 const kafka = new Kafka({
   clientId: 'lottery-api',
-  brokers: ['localhost:9092'],
+  brokers: [myBroker],
 })
 
 export { kafka }
